548431703a665ad97c5407258da9670a320efba3
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-18 19:25:17 +0600

    added welcome section and complet the extra two section requarments

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,35 @@
+import WelcomeBG from "../../assets/cover.jpg";
+import WelcomeCar from "../../assets/w-h.png";
 const Welcome = () => {
-  return <div>welcome to car master</div>;
+  const bannerStyle = {
+    backgroundImage: `url(${WelcomeBG})`,
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    <div style={bannerStyle}>
+      <div className="container mt-20 mx-auto flex flex-col md:flex-row-reverse justify-around">
+        <div className=" py-10 px-2">
+          <h3 className="text-4xl text-white  font-fontSquare">
+            Welcome to Car Master.
+          </h3>
+          <p className="max-w-[450px]">
+            {" "}
+            your ultimate destination for everything automotive. Dive into a
+            world of endless possibilities where you can search for your dream
+            car from our extensive listings, read reviews, and compare models
+            effortlessly. Our dynamic community is here to share their
+            experiences, and our expert content keeps you informed about the
+            latest trends and industry news. Ready to take the wheel? Start your
+            journey with us today and unlock the full potential of your
+            automotive passion
+          </p>
+        </div>
+        <div className=" flex items-center justify-center">
+          <img className="" src={WelcomeCar} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../../components/Banner/Banner";
 import BrandContainer from "../../components/BrandContainer/BrandContainer";
 import CarBuy from "../../components/CarBuy/CarBuy";
+import Welcome from "../../components/Welcome/Welcome";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <BrandContainer></BrandContainer>
+      <Welcome></Welcome>
       <CarBuy></CarBuy>
     </div>
   );
